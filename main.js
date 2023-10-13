a4d2f187ddc9af1fc5e7c63d4160d95b95eabad5
wh1t3tea <anna.maslova748@gmail.com>
2023-10-13 20:31:14 +0300

    part_2

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const menuBtn = document.querySelector(".menu__btn");
 const menuList = document.querySelector(".menu__list");
+const portfolioBtn = document.querySelector(".button-59");
+const portfolioList = document.querySelector(".portfolio_info")
 function toggleMenuVisibility() {
 menuList.classList.toggle("hide");
 }
 menuBtn.addEventListener("click", toggleMenuVisibility);
+function togglePortfolioVisibility() {
+portfolioList.classList.toggle("hide");
+}
+portfolioBtn.addEventListener("click", togglePortfolioVisibility);
